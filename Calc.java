2e8004436d430039b2b088569ccceee3ae5b1431
mersad-markoni <57413481+mersad-markoni@users.noreply.github.com>
2019-11-08 18:36:13 +0100

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -10,28 +10,43 @@ public class Calc {
     public static void main(String[] args) {
         int z = 1;
         while (z == 1) {
-
-            int a;
-            int b;
-            int c;
-            int r;
-            double rd;
+            int a=0;
+            int b=0;
+            int c=0;
+            int r=0;
+            double rd=0;
             Scanner in = new Scanner(System.in);
             System.out.println("Input number 1");
-            a = in.nextInt();
+            try {
+                a = in.nextInt();
+            }
+            catch (Exception e){
+                System.out.println("Input value is not a number!");
+                continue;
+            }
             System.out.println("You entered number " + a);
 
             System.out.println("Input number 2");
-            b = in.nextInt();
+            try {
+                b = in.nextInt();
+            }
+            catch (Exception e){
+                System.out.println("Input value is not a number!");
+                continue;
+            }
             System.out.println("You entered number " + b);
-
             System.out.println("Select math function:");
             System.out.println("1 - Add");
             System.out.println("2 - Substract");
             System.out.println("3 - Divide");
             System.out.println("4 - Multiply");
-            c = in.nextInt();
-
+            try {
+                c = in.nextInt();
+            }
+            catch (Exception e){
+                System.out.println("Input value is not a number!");
+                continue;
+            }
             if (c == 1) {
                 r = a + b;
                 System.out.println("The result: " + a + "+" + b + "=" + r);
@@ -43,7 +58,7 @@ public class Calc {
             if (c == 3) {
                 try {
                     rd = (double)a/b;
-                    System.out.println("The result: %.2f\n",rd);
+                    System.out.printf("The result: %.2f\n",rd);
                 } catch (Exception e) {
                     System.out.println("Division not possible!!");
                 }
@@ -58,8 +73,13 @@ public class Calc {
             System.out.println("Do you want to try again?");
             System.out.println("1 - Yes");
             System.out.println("2 - No");
-
-            z = in.nextInt();
+            try {
+                z = in.nextInt();
+            }
+            catch (Exception e){
+                System.out.println("Input value is not a number!");
+                continue;
+            }
             if (z == 2) break;
 
         }
